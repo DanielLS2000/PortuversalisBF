929475ccd5a7d8bede3e1571fdc0ca22cd2ed605
Daniel <daniellimas2000@gmail.com>
2024-05-18 12:15:53 -0300

    missions capped

--- common/defines/rb.defines.lua
+++ common/defines/rb.defines.lua
@@ -1,6 +1,6 @@
 -- AI is incompetent in fighting corruption with default budget set to 0.25 max.
 NDefines.NAI.CORRUPTION_BUDGET_FRACTION = 0.5
 -- Down from 0.5 to make AI more willing to release vassals.
-NDefines.NAI.GOVERNING_CAPACITY_OVER_PERCENTAGE_TOLERATED = 0.2
+NDefines.NAI.GOVERNING_CAPACITY_OVER_PERCENTAGE_TOLERATED = 0.5
 -- Twice costly corruption cost, but slider goes twice as far so the relative price will remain the same.
 NDefines.NCountry.CORRUPTION_COST = 0.1
